20e2983ae877b707e0aa1ecd280e1f7cb62110d4
ManjushaQA <32263961+ManjushaQA@users.noreply.github.com>
2017-10-16 17:28:34 +0530

    Update Login.java

--- LetterHub/src/test/java/dharani/LetterHub/Login.java
+++ LetterHub/src/test/java/dharani/LetterHub/Login.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
 import cucumber.api.DataTable;
-import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Given;po
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
